b339add089259bc1ba4d15b11f2e81ebf8f7a6c0
Ramirojj <126816654+Ramirojj@users.noreply.github.com>
2024-10-26 22:08:48 -0500

    [1.05] Display 1st character

--- Program.cs
+++ Program.cs
@@ -8,7 +8,11 @@ List<Character> characters = [];
 characters.AddRange(dks);
 characters.AddRange(marios);
 // display all characters
-foreach(Character character in characters)
+
+/*foreach(Character character in characters)
 {
   Console.WriteLine(character.Display());
-}
+}*/
+Console.Clear();
+// display first character
+Console.WriteLine(characters.First().Display());
