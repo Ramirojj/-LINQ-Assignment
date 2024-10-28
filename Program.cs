1e25f6523c8387064721bd02c39d567ad2414e54
Ramirojj <126816654+Ramirojj@users.noreply.github.com>
2024-10-27 21:29:23 -0500

    [1.12] Display whether or not there are any characters created in 1995

--- Program.cs
+++ Program.cs
@@ -32,4 +32,7 @@ Console.WriteLine(characters.First().Display());
 //Console.WriteLine(characters.Last().Display());
 //Console.WriteLine(characters.Min(c => c.YearCreated));
 // oldest character
-Console.WriteLine(characters.First(c => c.YearCreated == characters.Min(c => c.YearCreated)).Display());
+//Console.WriteLine(characters.First(c => c.YearCreated == characters.Min(c => c.YearCreated)).Display());
+// are there any characters created in 1995?
+bool Character1995 = characters.Any(c => c.YearCreated == 1995);
+Console.WriteLine($"Are there characters created in 1995: {Character1995}");
