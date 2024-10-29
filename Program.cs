eaae47018c14665536bf164ef961130a58548e02
Ramirojj <126816654+Ramirojj@users.noreply.github.com>
2024-10-28 19:52:21 -0500

    [1.25e] How many character(s) in the Mario series are something other than Human or Koopa species?

--- Program.cs
+++ Program.cs
@@ -270,6 +270,9 @@ Console.WriteLine($"are characters with no alias in the Donkey Kong series: {ali
 //int species = characters.Count(c => c.Species == "Koopa" && c.Series.Contains("Mario"));
 //Console.WriteLine($"there are {species} characters in the Mario series with a species of Koopa");
 ///[1.25d] List the character(s) in the Mario series that are Koopa species - return character name only.
-foreach (var character in characters.Where(c => c.Species == "Koopa" && c.Series.Contains("Mario")))  
-{Console.WriteLine($"name:{character.Name}");}
+//foreach (var character in characters.Where(c => c.Species == "Koopa" && c.Series.Contains("Mario")))  
+//{Console.WriteLine($"name:{character.Name}");}
 
+//[1.25e] How many character(s) in the Mario series are something other than Human or Koopa species?
+int species = characters.Count(c => c.Species != "Human" && c.Species != "Koopa" && c.Series.Contains("Mario"));
+Console.WriteLine($"there are {species} characters in the Mario series with a species other than Human or Koopa");
